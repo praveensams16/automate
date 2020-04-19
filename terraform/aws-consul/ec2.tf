2330b87ca7034b6c16d502ee435dc3c89f43a738
praveensam <praveen@local.in>
2020-04-19 14:13:30 +0300

    Sun Apr 19 14:13:30 +03 2020

--- terraform/aws-consul/ec2.tf
+++ terraform/aws-consul/ec2.tf
@@ -1,6 +1,6 @@
 module "ec2" {
     source = "./module/ec2"
-    location = data.aws_ami.ubuntu.id
+    location = var.images
     subnet = aws_subnet.private.id
     sc = aws_security_group.instance.id
     key = "praveen"
@@ -9,7 +9,7 @@ module "ec2" {
 
 module "ec21" {
     source = "./module/ec2"
-    location = data.aws_ami.ubuntu.id
+    location = var.images
     subnet = aws_subnet.private.id
     key = "praveen"
     sc = aws_security_group.instance.id
--- terraform/aws-consul/variable.tf
+++ terraform/aws-consul/variable.tf
@@ -1,10 +1,6 @@
-variable "image" {
-    type = map
-    default = {
-       us-east-1 =  "data.aws_ami.ubuntu.id"
-       us-east-2 = "ami-0f90a34c9df977efb"
-       }
-       }
+
+variable "images" {
+        }
 
 variable "region" {
     default = "us-east-1"
