84f95cddf1fb3130d882751de15f986461d34c62
praveensam <praveen@local.in>
2020-04-21 20:25:55 +0300

    Tue Apr 21 20:25:55 +03 2020

---
+++ chart/build/php/your-script.php
@@ -0,0 +1,14 @@
+<?php
+require_once('simpletest/autorun.php');
+require_once('../classes/log.php');
+
+class TestOfLogging extends UnitTestCase {
+    function testLogCreatesNewFileOnFirstMessage() {
+        @unlink('/temp/test.log');
+        $log = new Log('/temp/test.log');
+        $this->assertFalse(file_exists('/temp/test.log'));
+        $log->message('Should write this to a file');
+        $this->assertTrue(file_exists('/temp/test.log'));
+    }
+}
+?>
