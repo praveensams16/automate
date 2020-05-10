7bae565b62af6e604fccd2c3d6350732014d090a
praveensam <praveen@local.in>
2020-05-11 00:38:47 +0300

    Mon May 11 00:38:47 +03 2020

---
+++ golang/regex-commands.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+  "log"
+  "os/exec"
+  "fmt"
+  "strings"
+  "regexp"
+  )
+
+func main() {
+    cmd := exec.Command("ls", "-lah")
+    out, err := cmd.CombinedOutput()
+    if err != nil {
+        log.Fatalf("cmd.Run() failed with %s\n", err)
+    }
+    h:=string(out)
+    s:=strings.Split(h,"\n")
+    for i:=0;i<len(s);i++ {
+      k,_:=regexp.MatchString("ba.h",s[i])
+      if k {
+      fmt.Println("-->" + s[i])
+      }
+      }
+}
