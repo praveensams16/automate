2e50ebc57c54825037d0dde88a88f5c16203e35d
praveensam <praveensam@sedintechnology.com>
2020-07-11 12:26:07 +0300

    adding

--- terraform/autoscalling-launch/image.tf
+++ terraform/autoscalling-launch/image.tf
@@ -4,10 +4,11 @@ locals {
   timestamp_sanitized = "${replace("${local.timestamp}", "/[- TZ:]/", "")}"
 }
 
+# Adding image 
 data "aws_instance" "sam" {
   instance_tags = {
     clone = "yes"
-    }  
+  }  
      }
 
 resource "aws_ami_from_instance" "stage" {
@@ -15,4 +16,4 @@ resource "aws_ami_from_instance" "stage" {
   source_instance_id = data.aws_instance.sam.id
   snapshot_without_reboot = true
   }
-
+#Adding files
