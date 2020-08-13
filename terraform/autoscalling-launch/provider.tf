f2acb50484a4027f308b49e8b737ce0593889e3a
PraveenSam <praveensams16@gmail.com>
2020-08-13 17:34:00 +0300

    Thu Aug 13 17:34:00 +03 2020

--- terraform/autoscalling-launch/provider.tf
+++ terraform/autoscalling-launch/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
  region                  = var.region
- shared_credentials_file = "/root/.aws/creds"
+ shared_credentials_file = "/Users/laksh/.aws/creds"
  profile                 = "default"
       }
